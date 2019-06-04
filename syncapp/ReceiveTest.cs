fce2559fa87586ccae6ab112acbb90891038160e
tomorinao <@tomoinao>
2019-06-04 12:00:09 +0900

    受信用クラスを追加

---
+++ syncapp/ReceiveTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace syncapp
+{
+    public class ReceiveTest
+    {
+        public ReceiveTest(int port)
+        {
+            string localIpString = "127.0.0.1";
+            System.Net.IPAddress localAddress =
+                System.Net.IPAddress.Parse(localIpString);
+            int localPort = port;
+
+            //UdpClientを作成し、ローカルエンドポイントにバインドする
+
+            System.Net.Sockets.UdpClient udp =
+                new System.Net.Sockets.UdpClient(new System.Net.IPEndPoint(localAddress, localPort));
+
+            for (; ; )
+            {
+                //データを受信する
+                System.Net.IPEndPoint remoteEP = null;
+                byte[] rcvBytes = udp.Receive(ref remoteEP);
+
+                //データを文字列に変換する
+                string rcvMsg = System.Text.Encoding.UTF8.GetString(rcvBytes);
+
+                //受信したデータと送信者の情報を表示する
+                Console.WriteLine("受信したデータ:{0}", rcvMsg);
+                Console.WriteLine("送信元アドレス:{0}/ポート番号:{1}",
+                    remoteEP.Address, remoteEP.Port);
+
+                
+                    break;
+               
+            }
+
+            //UdpClientを閉じる
+            udp.Close();
+
+
+        }
+    }
+}
