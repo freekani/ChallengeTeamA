f462923244be1d0e05f8b536f03ac0eb0b2565bc
SKyoKen <45055188+SkyoKen@users.noreply.github.com>
2019-12-20 10:46:19 +0900

    update

--- SonicPi_Test/SonicPi_Test_GetTempo/SonicPi_GetTempo.rb
+++ SonicPi_Test/SonicPi_Test_GetTempo/SonicPi_GetTempo.rb
@@ -7,12 +7,15 @@ note = [ [0,:C1,:D1,:E1,:F1,:G1,:A1,:B1],
          [0,:C5,:D5,:E5,:F5,:G5,:A5,:B5],
          [0,:C6,:D6,:E6,:F6,:G6,:A6,:B6],
          [0,:C7,:D7,:E7,:F7,:G7,:A7,:B7],]
-step =[]
+step = []
 octave = []
-duration =[]
+duration = []
+type=[]
+measure=[]
 BPM=240
-
+m=0
 n=0
+t=0
 #------------------------------------------------------------
 #load_CSV
 #------------------------------------------------------------
@@ -23,16 +26,22 @@ define :load_CSV do
   step=[]
   octave=[]
   duration=[]
+  type=[]
+  measure=[]
   i=0
   while (i<file.length)
     step[i]=file[i][0].to_i
     octave[i]=file[i][1].to_i
     duration[i]=file[i][2].to_i
-    i=i+1
+    type[i]=file[i][3].to_f
+    measure[i]=file[i][4].to_i
+    i+=1
   end
+  m=measure[n]
   print "step",step
   print "octave",octave
   print "duration",duration
+  print "type",type
   print "-------------LOAD SUCCEED-------------"
 end
 #------------------------------------------------------------
@@ -49,20 +58,46 @@ end
 live_loop :load_Tempo do
   use_real_time
   msg = sync "/osc/TEMPO"
-  play_MUSIC
+  #1”‚Å
+  t+=1
 end
+
 #------------------------------------------------------------
 #play_MUSIC
 #------------------------------------------------------------
 
 define :play_MUSIC  do
   use_bpm BPM
-  print n
-  play note[octave[n]][step[n]],release: duration[n]*0.5
-  sleep duration[n]*0.5
+  print n+1
+  if step[n]!=0
+    play note[octave[n]][step[n]],release: duration[n]*0.5
+    sleep duration[n]*0.5
+  end
   (n<step.length-1)? n+=1 : n=0
+  
+  
 end
 
+
 load_CSV
+#main
+live_loop :guit do
+  print t
+  
+  while t>0
+    
+    t-=1
+    i=0
+    while i<1
+      i+=type[n]
+      play_MUSIC
+    end
+    
+  end
+  sleep 0.1
+end
+
+
+
 
 
